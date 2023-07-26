32dff590773467e02d987597a3241ded80be571e
Munawar Hussain <munawarhussa.7@gmail.com>
2023-07-26 12:52:50 +0500

    Combine reducer for multiple reducers

--- src/redux/reducers/ExperienceReducer.js
+++ src/redux/reducers/ExperienceReducer.js
@@ -1,6 +1,8 @@
 const reducer = (state = 0, action) => {
+    
     if (action.type === 'increment') {
         return state + action.payload
+
     } else if (action.type === 'decrement') {
         if (state > 0) {
             return state - action.payload
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,4 +3,4 @@ import reducers from "../reducers";
 import thunk from "redux-thunk";
 
     
-export const store = createStore(reducers,{}, applyMiddleware(thunk))
+export const store = createStore(reducers,{}, applyMiddleware(thunk))   
