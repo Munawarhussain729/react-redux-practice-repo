9dfcc1b67bc02e3eae2ce3cad17736d84d71f90f
nageenhaider <nageen.haider2002@gmail.com>
2023-07-31 19:56:43 +0500

    redux,cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Home from './components/Home/Home';
-
+import Cart from './components/Cart/Cart';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import { Button } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
 import ProductDisplay from './ProductDisplay'
 import Cart from '../Cart/Cart'
-
+import { Link } from 'react-router-dom'
 function Home() {
     const [products, setProducts] = useState([])
     const Items = useSelector((state) => state.ProductReducer.cartItems)
@@ -29,7 +29,8 @@ function Home() {
             <div className='d-flex justify-content-between w-75' >
                 <h1 onClick={()=>setShowCart(false)} style={{cursor:"pointer"}}>Welcome to Learners Store</h1>
                 {
-                    Items?.length > 0 ? <Button onClick={()=>setShowCart(!showCart)}>Cart {Items.length}</Button> : <div></div>
+                    Items?.length > 0 ? <Link to="/cart"><Button>Cart {Items.length}</Button></Link> : <div></div>
+                    
                 }
             </div>
             {
